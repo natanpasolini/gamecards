6c5e957bc1030ef1a5a665ff085a2bdde164558b
Natan Pasolini <natanpasoliniob@gmail.com>
2026-01-09 23:51:12 -0300

    leitura de csv

--- src/main.js
+++ src/main.js
@@ -1,63 +1,100 @@
-const meusJogos = [{ year: "2026", title: "Warhammer 40k: Space Marine 2", img: "spacemarine2.jpg", hours: "20", achievements: "22", maxachievements: "50", platform: "steam", score: "S" }, { year: "2025", title: "Clair", img: "spacemarine2.jpg", hours: "57", achievements: "55", maxachievements: "55", platform: "steam", score: "SSS" }]
+async function carregarDados() {
+    const url = "./csv/jogos.csv";
+    const resposta = await fetch(url);
+    const dadosBrutos = await resposta.text();
+    
+    processarCSV(dadosBrutos);
+}
 
-meusJogos.forEach(jogo => {
-    console.log(`Eu zerei o jogo: ${jogo.year} ${jogo.title} ${jogo.img} ${jogo.hours} ${jogo.achievements} ${jogo.maxachievements} ${jogo.platform} ${jogo.score}`);
-    if (jogo.platform !== 'steam' && jogo.platform != 'emulador') {
-        jogo.platform = 'technology';
-    }
-    else if (jogo.platform == 'emulador') {
-        jogo.platform = 'gaming';
-    }
-    const gamecardHtml =
-    `<div class="rounded-xl flex flex-col py-6 px-4 shadow-md max-w-[400px] gamecard-bg">
-                    <div class="relative">
-                        <div class="px-3 py-0 m-0 bg-black border border-white rounded flex justify-center items-center absolute -rotate-30 select-none top-1">
-                            <span class="text-white text-3xl leading-none font-micro-5">
-                                ${jogo.year}
-                            </span>
-                        </div>
-                        <div class="w-full px-4 py-2">
-                            <div class="border-2 rounded">
-                                <div class="aspect-video flex justify-center">
-                                    <img src="./imgs/games/${jogo.img}" class="aspect-video border border-white rounded object-cover">
+function processarCSV(texto) {
+    const linhas = texto.split("\n").filter(linha => linha.trim() !== "");
+
+    const listaJogos = linhas.slice(1).map(linha => {
+        const valores = linha.replace(/\r/g, "").split(",");
+
+        return {
+            title: valores[0],
+            platform: valores[1],
+            achievements: valores[2] || "N/A",
+            maxachievements: valores[3] || "N/A",
+            hours: valores[4],
+            score: valores[5] || "N/A",
+            year: valores[6]
+        };
+    });
+
+    console.log(listaJogos);
+    renderizarCards(listaJogos);
+}
+
+carregarDados();
+function renderizarCards(jogos) {
+    jogos.forEach(jogo => {
+        jogo.platform = jogo.platform.toLowerCase();
+        let platformicon = jogo.platform;
+        let gamecardColor = 'purple';
+        let gamecardTextColor = 'white';
+        if (jogo.platform == 'emulador' || jogo.platform == 'switch') {
+            platformicon = 'gaming';
+        }
+        else if (jogo.platform !== 'steam' && jogo.platform != 'emulador') {
+            platformicon = 'technology';
+        }
+        if (jogo.achievements == jogo.maxachievements && jogo.maxachievements != 'N/A') {
+            gamecardColor = 'gold';
+            gamecardTextColor = 'gold';
+        }
+        const gamecardHtml =
+        `<div class="rounded-xl flex flex-col py-6 px-4 shadow-md max-w-[400px] gamecard-bg-${gamecardColor}">
+                        <div class="relative">
+                            <div class="px-3 py-0 m-0 bg-black border border-white rounded flex justify-center items-center absolute -rotate-30 select-none top-0">
+                                <span class="text-${gamecardTextColor} text-3xl leading-none font-micro-5">
+                                    ${jogo.year}
+                                </span>
+                            </div>
+                            <div class="w-full px-4 py-2">
+                                <div class="border-2 rounded">
+                                    <div class="aspect-video flex justify-center">
+                                        <img src="./imgs/games/${jogo.img}" class="aspect-video border border-white rounded object-cover">
+                                    </div>
                                 </div>
                             </div>
                         </div>
-                    </div>
-                    <div class="flex flex-col justify-center items-center">
-                        <div class="bg-black p-1 w-full justify-center items-center">
-                            <h1 class="text-white text-center text-base font-silkscreen">
-                                ${jogo.title}
-                            </h1>
-                        </div>
-                        <div class="grid grid-cols-2 w-full">
-                            <div class="flex flex-col items-start pt-1 border-2 gap-1">
-                                <div class="flex justify-start items-center pl-2 gap-2">
-                                    <i class="hn hn-trophy-solid text-yellow-300 text-3xl"></i>
-                                    <span class="text-white leading-none text-3xl font-micro-5" id="gamecard-achievements">
-                                        ${jogo.achievements}/${jogo.maxachievements}
-                                    </span>
+                        <div class="flex flex-col justify-center items-center">
+                            <div class="bg-black p-1 w-full justify-center items-center">
+                                <h1 class="text-${gamecardTextColor} text-center text-base font-silkscreen">
+                                    ${jogo.title}
+                                </h1>
+                            </div>
+                            <div class="grid grid-cols-2 w-full">
+                                <div class="flex flex-col items-start pt-1 border-2 gap-1">
+                                    <div class="flex justify-start items-center pl-2 gap-2">
+                                        <i class="hn hn-trophy-solid text-yellow-300 text-3xl text-stroke"></i>
+                                        <span class="text-${gamecardTextColor} leading-none text-3xl font-micro-5 text-stroke" id="gamecard-achievements">
+                                            ${jogo.achievements}/${jogo.maxachievements}
+                                        </span>
+                                    </div>
+                                    <div class="flex justify-start items-center pl-2 gap-2">
+                                        <i class="hn hn-clock text-yellow-300 text-3xl text-stroke"></i>
+                                        <span class="text-white leading-none text-3xl font-micro-5 text-stroke" id="gamecard-hours">
+                                            ${jogo.hours}h
+                                        </span>
+                                    </div>
                                 </div>
-                                <div class="flex justify-start items-center pl-2 gap-2">
-                                    <i class="hn hn-clock text-yellow-300 text-3xl"></i>
-                                    <span class="text-white leading-none text-3xl font-micro-5" id="gamecard-hours">
-                                        ${jogo.hours}h
-                                    </span>
+                                <div class="flex justify-center h-20">
+                                    <div class="w-full p-2 flex justify-center items-center border-2 border-x-0 platform-${jogo.platform}">
+                                        <i class="hn hn-${platformicon} text-3xl text-white"></i>
+                                    </div>
+                                    <div class="p-2 flex justify-center items-center w-full border-2 score-${jogo.score}">
+                                        <span class="text-3xl font-pixelify-sans">
+                                            ${jogo.score}
+                                        </span>
+                                    </div>
                                 </div>
                             </div>
-                            <div class="flex justify-center h-20">
-                                <div class="w-full p-2 flex justify-center items-center border-2 border-x-0 platform-${jogo.platform}">
-                                    <i class="hn hn-${jogo.platform} text-3xl text-white"></i>
-                                </div>
-                                <div class="p-2 flex justify-center items-center w-full border-2 score-${jogo.score}">
-                                    <span class="text-3xl font-pixelify-sans">
-                                        ${jogo.score}
-                                    </span>
-                                </div>
-                            </div>
                         </div>
-                    </div>
-                </div>`;
-    document.querySelector('#gamelist').innerHTML += gamecardHtml;
-});
+                    </div>`;
+        document.querySelector('#gamelist').innerHTML += gamecardHtml;
+    });
+}
 
